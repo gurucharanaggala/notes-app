d8e1d93e1334e8651760efacc2cdea4fa0d93960
Guru Charan Aggala <161590781+gurucharanaggala@users.noreply.github.com>
2025-07-08 10:21:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/notes-app/'
+  base: '/notes-app/',
   plugins: [react(), tailwindcss()],
 })
